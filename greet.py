c46892512b80373aa4fd231b028e7a07a005b910
lunaknight09 <130379665+lunaknight09@users.noreply.github.com>
2024-02-19 17:24:19 -0500

    docker upload

---
+++ greet.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 16 11:47:57 2024
+
+@author: nhopk
+"""
+
+import emoji
+
+def message():
+    return emoji.emojize(":waving_hand: :globe_showing_Americas:")
+
+if __name__ == "__main__":
+    print(message())
